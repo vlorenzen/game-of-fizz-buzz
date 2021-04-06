4b3cb5ae66a9c22100e6579c08506c81d507bcc5
Viktor <43089746+vlorenzen@users.noreply.github.com>
2021-04-06 23:34:06 +0200

    Basic Styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [gameStarted, setGameStarted] = useState(true);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to the game of FizzBuzz!</h1>
       </header>
+      <main>
+        {gameStarted ? (
+          <ul className="App-chat">
+            <li>1</li>
+            <li>2</li>
+            <li>Fizz</li>
+            <li>4</li>
+            <li>Buzz</li>
+            <li>Fizz</li>
+            <li>7</li>
+            <li>8</li>
+            <li>Fizz</li>
+            <li>Buzz</li>
+            <li>11</li>
+            <li>Fizz</li>
+            <li>13</li>
+            <li>14</li>
+            <li>FizzBuzz</li>
+          </ul>
+        ) : (
+          <>
+            <h3>Rules:</h3>
+            <ul>
+              <li>Counting starts from 1.</li>
+              <li>Players take turns incrementing the number by 1.</li>
+              <li>
+                If the number is divisible by 3, replace the number with the
+                word "Fizz".
+              </li>
+              <li>
+                If the number is divisible by 5, replace the number with the
+                word "buzz".
+              </li>
+              <li>
+                If the number is divisible by both, write the word "FizzBuzz".
+              </li>
+            </ul>
+            To start a new game, type <em>"start"</em>.
+            <br />
+            To check the previous score, type <em>"score"</em>.
+            <br />
+            To check the current highscore, type <em>"highscore"</em>.
+            <br />
+            To see the rules type <em>"rules"</em>.
+            <br />
+          </>
+        )}
+      </main>
+      <form className="App-user-input">
+        <input type="text"></input>
+        <button>send</button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
